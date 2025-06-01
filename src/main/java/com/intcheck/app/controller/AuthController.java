644f3289482783029bfeb92f8315f85e4e4da046
paccoc43 <pacotecnolofuerte@gmail.com>
2025-06-02 01:11:01 +0200

    Log implementado en AuthService

--- src/main/java/com/intcheck/app/controller/AuthController.java
+++ src/main/java/com/intcheck/app/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.intcheck.app.controller;
 import com.intcheck.app.modelo.AuthRequest;
 import com.intcheck.app.modelo.Usuario;
 import com.intcheck.app.services.AuthService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired
     private AuthService authService;
 
@@ -20,11 +24,13 @@ public class AuthController {
         try {
             Usuario usuarioAutenticado = authService.autenticar(authRequest.getUsuario(), authRequest.getPassword());
             if (usuarioAutenticado != null) {
+                logger.info("Usuario autenticado: {}", usuarioAutenticado.getNombre_usuario());
                 return ResponseEntity.ok(usuarioAutenticado);
             } else {
                 return ResponseEntity.status(401).body("Credenciales incorrectas");
             }
         } catch (Exception e) {
+            logger.error("Error al iniciar el servicio de autenticacion", e);
             throw new RuntimeException("Error al iniciar el servicio de autenticacion",e);
         }
     }
